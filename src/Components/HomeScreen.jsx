fd7d24f5d6392401fa0ca99b074b6d72134db4af
shiv0023 <shivkashyap808@gmail.com>
2024-05-24 11:07:36 +0530

    addLinerGradient

--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -1,13 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import AntDesign from 'react-native-vector-icons/dist/AntDesign';
+import LinearGradient from "react-native-linear-gradient";
 
 
 export function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home! </Text>
-      <AntDesign name={'arrowright'} size={45} color={"red"}/>
-    </View>
+    <LinearGradient colors={['#FDF0F3', '#FDF0F3', '#FFFBFC']} style={styles.container}>
+    <Text style={styles.buttonText}>
+      Sign in with Facebook
+    </Text>
+  </LinearGradient>
   );
 }
+const styles=StyleSheet.create({
+  container:{
+flex:1
+  }
+})
